637cabf1e5c9d1298893c13644bb10ede701eeb9
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-07-13 14:36:48 +0200

    Aggiunta classe pizza e view con la tabella in cui è presente la lista di pizze create

--- Pizza.cs
+++ Pizza.cs
@@ -2,5 +2,17 @@
 {
     public class Pizza
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public float Price { get; set; }
+
+        public Pizza(string name, string description, string image, float price)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
     }
 }
